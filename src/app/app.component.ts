15442b3ab658da7e3c22f12c41a8163fc67fae90
Christian Schmitt <christian.schmitt@iunera.com>
2024-12-17 13:38:31 +0100

    working design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDrawerContainer, MatDrawerContent} from '@angular/material/sidenav';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    MatButtonModule, // For Material buttons
+    MatCardModule,
+    MatDrawerContainer,
+    MatDrawerContent,
+    NgOptimizedImage,
+    // For Material cards
+  ],
 })
+
 export class AppComponent {
   title = 'license-token-website';
 }

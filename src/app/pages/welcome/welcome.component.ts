4f27591b54efe1e178196c7712d92e23f3bb52c6
Christian Schmitt <christian.schmitt@iunera.com>
2024-12-19 14:21:52 +0100

    Update

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -4,6 +4,8 @@ import {MatDrawerContainer, MatDrawerContent} from "@angular/material/sidenav";
 import {MatIcon} from "@angular/material/icon";
 import {MatCardModule} from '@angular/material/card';
 import {Router} from "@angular/router"
+import {MatList, MatListItem} from '@angular/material/list';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
@@ -16,6 +18,9 @@ import {Router} from "@angular/router"
     MatCardModule,
     MatDrawerContainer,
     MatDrawerContent,
+    MatList,
+    MatListItem,
+    NgForOf,
   ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
@@ -36,7 +41,53 @@ export class WelcomeComponent {
   }
 
   navigateToDonate() {
-    this.router.navigate(['/more']); // Navigate to Donate on OpenSea route
+    window.open('https://nftpuzzle.license-token.com/', '_blank');
   }
 
+  whyItems = [
+    { icon: 'task_alt', text: 'You have an open-source project and develop it in your private time?' },
+    { icon: 'task_alt', text: 'People are using your code, and you want to get paid for it?' },
+    { icon: 'task_alt', text: 'You are an indie-hacker?' },
+    { icon: 'task_alt', text: 'Large corporations are using or reselling your code, and you get nothing from it?' },
+    { icon: 'task_alt', text: 'You want to be able to live from your open-source work?' }
+  ];
+
 }
+
+// change to
+//
+// <mat-card class="why-card">
+//   <mat-card-header>
+//   <mat-card-title>Speed</mat-card-title>
+//   </mat-card-header>
+//   <mat-card-content>
+//   <p>Unmatched scalability and performance, lightning fast!</p>
+// </mat-card-content>
+// </mat-card>
+
+
+
+// You have a open source project and develop it in your private time?
+//   People are using your code and you want to get paid for it?
+//                                                             You are a indien-hacker?
+//   Large cooperations are using or reselling your code and you get nothing from it?
+//   You want to be able to live from your open source work?
+//
+//
+//   what does it for me check
+// <!-- Warum macht das die Kunden glücklich -->
+// what does it for my users -
+// What it does for your users! can use reliable projects
+// clear estimation about what it costs
+// transparency
+// security who was working on the project. Proof of ownership
+//
+//
+// <!-- warum ist das geil für die OS Community -->
+// geld verdienen an vorhandenen Projekten
+// find supporters more easily
+// non-profit use is still free
+//
+// <!-- warum bringt es die Welt voran-->
+// and where do you take me
+

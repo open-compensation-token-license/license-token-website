a59bd23707a9580912df68752b29f4c565202b85
Christian Schmitt <christian.schmitt@iunera.com>
2024-12-20 09:44:52 +0100

    finalize text

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {WelcomeComponent} from './pages/welcome/welcome.component';
 import {MoreComponent} from './pages/more/more.component';
+import {ApplyLicenseComponent} from './pages/apply-license/apply-license.component';
 
 export const routes: Routes = [
-
-  { path: '', component: WelcomeComponent },
-  { path: 'more', component: MoreComponent }, // New route for More Page
-
+  {path: '', component: WelcomeComponent},
+  // {path: 'more', component: MoreComponent},
+  // {path: 'apply-license', component: ApplyLicenseComponent}
 ];
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {Component,} from '@angular/core';
 import {MatButton, MatButtonModule} from "@angular/material/button";
 import {MatDrawerContainer, MatDrawerContent} from "@angular/material/sidenav";
 import {MatIcon} from "@angular/material/icon";
 import {MatCardModule} from '@angular/material/card';
 import {Router} from "@angular/router"
 import {CtaComponent} from '../cta/cta.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
@@ -17,11 +18,12 @@ import {CtaComponent} from '../cta/cta.component';
     MatCardModule,
     MatDrawerContainer,
     MatDrawerContent,
-    CtaComponent,
+    NgForOf,
   ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
+
 export class WelcomeComponent {
   constructor(private router: Router) {
   }
@@ -34,117 +36,117 @@ export class WelcomeComponent {
   }
 
   navigateToApplyLicense() {
-    this.router.navigate(['/apply-license']); // Navigate to Apply License route
+    this.router.navigate(['/apply-license']);
+  }
+
+
+  contactUs() {
+    window.open('mailto:support@example.com', '_self');
+    // window.location.href = "mailto:abc@abc.com?subject=files"
+
   }
 
   navigateToDonate() {
     window.open('https://nftpuzzle.license-token.com/', '_blank');
   }
 
-  // language=HTML
-  description: string = `<p>
-    The <b>Open Compensation Token License (OCTL)</b> empowers developers to monetize their open-source work
-    while supporting free non-commercial and community-based usage. It provides a unique licensing mechanism
-    with compensation tokens, ensuring fair payment for contributors and bridging the gap between corporate profits
-    and open-source contributions. OCTL promotes transparency, ownership, and financial opportunities for developers,
-    helping grow the open-source ecosystem sustainably.
-  </p>
-  `;
+  why = {
 
-  // language=HTML
-  forYou = `
-    The <b>Open Compensation Token License
-      (OCTL)</b> offers significant benefits to maintainers, developers, and project owners, addressing key challenges in open-source development:
-    <p><b>Fair Compensation:</b> Maintainers get paid when their work is commercially used, ensuring they can
-        financially sustain themselves while focusing on developing their projects.</p>
-    <p><b>Control Over Licensing:</b> The OCTL empowers project owners to set transparent terms for commercial use
-        while still allowing free use for non-commercial contributions.</p>
-    <p><b>Encourages Contribution:</b> Developers no longer need to sacrifice financial stability to maintain an
-        open-source project, motivating continuous updates, improvements, and innovation.</p>
-    <p><b>Increased Recognition:</b> The token model provides clear attribution and proof of ownership, helping
-        maintainers gain visibility and respect in the community.</p>
-    <p><b>Sustainable Future:</b> By monetizing corporate use, maintainers avoid burnout and can thrive while
-        contributing to technology and society.</p>
-    The license ensures maintainers are rewarded fairly without compromising their open-source principles.
-  `;
+    title: 'What does it?',
+    // language=HTML max_line_length = 80
+    description: `<p>
+      The <b>Open Compensation Token License (OCTL)</b> provides a unique blockchain based licensing mechanism. This ensures fair and transparent payment for
+      contributors, while allowing the source code to be publicly available and to be re-used or advanced in other projects. As a result of this a naturally
+      sustainable ecosystem of public available source code fostered sustainably.
+    </p>`,
 
-  // language=HTML
-  forUsers: string = `
-    <b>The Open Compensation Token License (OCTL)</b> is beneficial for users because it ensures access to
-    high-quality, sustainable open-source projects. Here's why it's good for users:
-    <p><b>Enhanced Project Reliability:</b> By compensating developers, projects are better maintained, regularly
-      updated, and more secure, providing users with reliable software.</p>
+    cards: [
+      {
+        title: 'Whats in it for you?',
+        icon: 'person',
+        // language=HTML
+        description: `
+          <p>How Token-Based Licensing Supports Your Work</p>
+          <p>Your source code can still be available in the web a without having to worry about copycats.</p>
+          <p>You'll receive an income stream from the foundation of your code: getting paid when your source code is
+            commercially applied while ensuring that forking and collaborations remain possible and your development efforts are not in vain.</p>
+          <p>Even though you earn from commercial applications, you still give back to the community by allowing free use for non-commercial contributions.</p>
+          <p>Increased recognition and transparency: The token model provides clear attribution and proof of ownership, enabling maintainers to gain visibility
+            and respect within the community.</p>
+        `
+      },
+      {
+        title: 'Supporting your customers',
+        icon: 'people',
+        // language=HTML
+        description: `
+          <b>The Open Compensation Token License
+            (OCTL)</b> is beneficial for users because it ensures access to high-quality, sustainable open-source projects Here's why it's good for users:
+          <p><b>Enhanced Project Reliability:</b> By compensating developers, projects are better maintained, regularly updated, and more secure, providing
+            users with reliable software.</p>
+          <p><b>Increased Transparency:</b> Project ownership and contributions are transparently tracked with the blockchain. </p>
+          <p><b>Encourages Innovation:</b> Fair compensation motivates developers to invest more effort and creativity, resulting in better features and
+            functionality for users and faster innovation cycles</p>
+        `
+      },
+      {
+        title: 'Creates a better world',
+        icon: 'public',
+        // language=HTML
+        description: `
+          <p> The <b>Open Compensation Token License (OCTL)</b> fosters a sustainable open-source ecosystem: </p>
+          <p><strong>Sustainability of open source:</strong> The abandonment of valuable projects is reduced, because of
+            direct support of the used projects. This ensures long-term support and continuous improvement.</p>
+          <p><strong>Open Innovation:</strong> It encourages contributions to open-source, advancing technology faster and more collaboratively.</p>
+          <p><strong>Access for All:</strong> Non-commercial use remains free, ensuring equitable access to tools and knowledge, empowering communities
+            worldwide.</p>
+        `
+      }
+    ]
+  }
 
-    <p><b>Transparency:</b> The license ensures transparency about project ownership and contributions, giving
-      users confidence in the tools they use.</p>
 
-    <p><b>Encourages Innovation:</b> Fair compensation motivates developers to invest more effort and creativity,
-      resulting in better features and functionality for users.</p>
+  knowFrom = {
+    title: 'You know our tech from',
+    subTitle: 'Our work has been recognized and featured by industry-leading platforms:',
+    points: [
+      {
+        title: 'Fahrbar20',
+        link: 'https://www.iunera.com/kraken/big-data-examples/how-nfts-and-ai-can-solve-public-transport-challenges/',
+        image: '../assets/images/logo-fahrbar20.svg',
+        alt: 'Fahrbar20',
+        subtitle: 'NFTs and AI solving public transport challenges',
+      },
+      {
+        title: 'Tracking, Prediction and Analytics',
+        link: 'https://www.iunera.com/kraken/big-data-examples/third-party-data-geotracking-coronavirus/',
+        image: '../assets/images/logo-trackingapp.png',
+        alt: 'Tracking App',
+        subtitle: 'Insights from Covid pandemic tracking apps',
+      },
+      {
+        title: 'AI Social Altcoin Analytics',
+        link: 'https://www.youtube.com/watch?v=4tv_L7W76b8',
+        image: '../assets/images/logo-scoinanalytics.png',
+        alt: 'Scoinanalytics',
+        subtitle: 'AI-powered altcoin trends and tweetbot'
+      },
+      {
+        title: 'AI Analytics of German Federal Election',
+        link: 'https://www.iunera.com/kraken/projects/baden-tv-interview-dr-frey-bundestagswahlanalyse/',
+        image: '../assets/images/logo-bundestagswahlanalyse.png',
+        alt: 'Election Analytics',
+        subtitle: 'In-depth election analysis powered by AI'
+      },
+      {
+        title: 'Swarm Travel Guide Apps',
+        link: 'https://www.chip.de/downloads/Stappz-Android-App_74740571.html',
+        image: '../assets/images/logo-stappz.png',
+        alt: 'Stappz',
+        subtitle: 'Discover destinations with travel guide apps'
+      },
+    ],
 
-    <p><b>Free for Non-Commercial Use:</b> Users contributing to the community or non-commercially using the
-      software can still benefit from free access.</p>
 
-    <p><b>Future-Proofing:</b> Sustainable open-source development benefits users in the long term by ensuring
-      projects are not abandoned.</p>
-    <p>This balance ensures that users benefit from robust, well-supported software.</p>
-  `;
-
-  // language=HTML
-  forTheWorld: string = `
-    <p>
-      The <b>Open Compensation Token License (OCTL)</b> fosters a sustainable open-source ecosystem:
-    </p>
-    <p><strong>Empowering Developers:</strong> It ensures creators are fairly compensated for their work,
-    enabling them to dedicate more time and effort to their solutions.</p>
-    <p><strong>Sustainability of Open-Source:</strong> By monetizing commercial use, OCTL prevents the
-    abandonment of valuable projects, ensuring long-term support and continuous improvement.</p>
-    <p><strong>Bridging Corporate Responsibility:</strong> It holds corporations accountable for profiting
-    from open-source while supporting fair collaboration with the developer community.</p>
-    <p><strong>Open Innovation:</strong> It encourages contributions to open-source, advancing
-    technology faster and more collaboratively.</p>
-    <p><strong>Access for All:</strong> Non-commercial use remains free, ensuring equitable access to
-    tools and knowledge, empowering communities worldwide.</p>
-    <p>
-      OCTL aligns innovation, fairness, and sustainability to drive global progress.
-    </p>
-  `;
-
+  }
 }
-
-
-// change to
-//
-// <mat-card class="why-card">
-//   <mat-card-header>
-//   <mat-card-title>Speed</mat-card-title>
-//   </mat-card-header>
-//   <mat-card-content>
-//   <p>Unmatched scalability and performance, lightning fast!</p>
-// </mat-card-content>
-// </mat-card>
-
-
-// You have a open source project and develop it in your private time?
-//   People are using your code and you want to get paid for it?
-//                                                             You are a indien-hacker?
-//   Large cooperations are using or reselling your code and you get nothing from it?
-//   You want to be able to live from your open source work?
-//
-//
-//   what does it for me check
-// <!-- Warum macht das die Kunden glücklich -->
-// what does it for my users -
-// What it does for your users! can use reliable projects
-// clear estimation about what it costs
-// transparency
-// security who was working on the project. Proof of ownership
-//
-//
-// <!-- warum ist das geil für die OS Community -->
-// geld verdienen an vorhandenen Projekten
-// find supporters more easily
-// non-profit use is still free
-//
-// <!-- warum bringt es die Welt voran-->
-// and where do you take me
-
